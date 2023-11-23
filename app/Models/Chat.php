8c69815cc8b3198135747a795b6df32e4cafd33c
alexkudzul <alexkudzul@gmail.com>
2023-11-23 13:44:53 -0600

    Add mutators to chat model

--- app/Models/Chat.php
+++ app/Models/Chat.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Storage;
 
 class Chat extends Model
 {
@@ -15,6 +17,41 @@ class Chat extends Model
         'is_group',
     ];
 
+
+    /*
+     * Attributes
+     */
+
+    public function name(): Attribute
+    {
+        return new Attribute(
+            get: function ($value) {
+                if ($this->is_group) {
+                    return $value;
+                }
+
+                $user = $this->users->where('id', '!=', auth()->id())->first();
+                $contact = auth()->user()->contacts()->where('contact_id', $user->id)->first();
+
+                return $contact ? $contact->name : $user->email;
+            }
+        );
+    }
+
+    public function image(): Attribute
+    {
+        return new Attribute(
+            get: function () {
+                if ($this->is_group) {
+                    return Storage::url($this->image_url);
+                }
+
+                $user = $this->users->where('id', '!=', auth()->id())->first();
+
+                return $user->profile_photo_url;
+            }
+        );
+    }
 
     /*
      * Relationships
--- resources/views/livewire/chat/chat-component.blade.php
+++ resources/views/livewire/chat/chat-component.blade.php
@@ -46,13 +46,22 @@
                 @if ($contactChat || $chat)
                     <div class="bg-gray-100 h-16 flex items-center px-3">
                         <figure>
-                            <img class="w-10 h-10 rounded-full object-cover object-center"
-                                src="{{ $contactChat->user->profile_photo_url }}" alt="{{ $contactChat->name }}">
+                            @if ($chat)
+                                <img class="w-10 h-10 rounded-full object-cover object-center" src="{{ $chat->image }}"
+                                    alt="{{ $chat->name }}">
+                            @else
+                                <img class="w-10 h-10 rounded-full object-cover object-center"
+                                    src="{{ $contactChat->user->profile_photo_url }}" alt="{{ $contactChat->name }}">
+                            @endif
                         </figure>
 
                         <div class="ml-4">
                             <p class="text-gray-800">
-                                {{ $contactChat->name }}
+                                @if ($chat)
+                                    {{ $chat->name }}
+                                @else
+                                    {{ $contactChat->name }}
+                                @endif
                             </p>
                             <p class="text-green-500 text-xs">
                                 Online
